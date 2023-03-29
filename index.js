70dc652dcfad785c8071f0760bf721f94bd1cb62
BrunoNascimento02 <brunonascimentophotograph@gmail.com>
2023-03-29 14:39:39 -0300

    d

---
+++ index.js
@@ -0,0 +1,7 @@
+function oneclick(){
+    var menu =
+        document.getElementById('ul');
+        if (menu.style.display == "block"){
+            menu.style.display = "none"
+        } else menu.style.display = "block"
+    }
